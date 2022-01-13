a971e377f5b4b78d0408d45ddce4172a3e139f2e
Igor Shapoval <azzgard1992@gmail.com>
2022-01-13 18:18:28 +0200

    +

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let colorId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  colorId = setInterval(() => getRandomHexBackgroundColorBody(), 1000);
+  refs.startBtn.setAttribute('disabled', 'disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(colorId);
+  refs.startBtn.removeAttribute('disabled', 'disabled');
+}
+
+function getRandomHexBackgroundColorBody() {
+  return (refs.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(
+    16,
+  )}`);
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onClickBtn);
+
+function onClickBtn(e) {
+  e.preventDefault();
+
+  let position = 0;
+  let delay = Number(e.currentTarget.delay.value);
+  let step = Number(e.currentTarget.step.value);
+  let amount = Number(e.currentTarget.amount.value);
+
+  setInterval(() => {
+    if (position >= amount) {
+      return;
+    }
+    position += 1;
+
+    setTimeout(() => {
+      delay += step;
+    });
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }, step);
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
